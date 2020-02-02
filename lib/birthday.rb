89cd008f112783722691e948d73a2bec4b9cac37
Edward Williams <eddie_w38@yahoo.com>
2020-02-02 04:47:08 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -8,10 +8,12 @@ birthday_kids = {
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  end
-else 
+    
+     else 
+  
   puts "You are too old for this."
-end
- 
+    end
+  end
+end 
 
 
